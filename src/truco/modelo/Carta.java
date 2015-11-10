576b0b9241f39c8b056f6baa6d1d5251494170a5
arnoldclq3 <arnoldclq3@gmail.com>
2015-11-10 18:54:57 -0300

    TestCarta | subo test, y el codigo minimo que hace pasar lo test

--- src/truco/modelo/Carta.java
+++ src/truco/modelo/Carta.java
@@ -2,8 +2,23 @@ package truco.modelo;
 
 public class Carta {
 
+	private int valor;
+	private String palo;
+	
 	public Carta(int valor, String palo) {
-		// TODO Auto-generated constructor stub
+		
+		this.valor = valor;
+		this.palo = palo;
+	}
+	
+	boolean mismoPalo(Carta carta) {
+		
+		return ( this.palo == carta.palo );
+	}
+	
+	int getValor() {
+		
+		return this.valor;
 	}
 
 }
---
+++ test/truco/modelo/CartaTest.java
@@ -0,0 +1,26 @@
+package truco.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CartaTest {
+	
+	private Carta miCarta;
+
+	@Before
+	public void setup() {
+		
+		this.miCarta = new Carta(1,"espada");
+	}
+	
+	@Test
+	public void testCartaDelMismoPaloDevuelveTrue() {
+		
+		Carta otraCarta = new Carta(7,"espada");
+		
+		assertTrue(miCarta.mismoPalo(otraCarta));
+	}
+
+}
