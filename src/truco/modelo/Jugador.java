c8256c1a33cf6090ec90b26b8742f6eee021a569
Sebysl <sebas_out@hotmail.com>
2015-11-10 20:20:03 -0300

    [Jugador] Pruebas para Puntaje Envido

--- src/truco/modelo/Jugador.java
+++ src/truco/modelo/Jugador.java
@@ -1,15 +1,47 @@
 package truco.modelo;
 
+import java.util.ArrayList;
+
 public class Jugador {
+	
+	// carta.mismoPalo
 
-	public void nuevaCarta(Carta carta) {
-		// TODO Auto-generated method stub
-		
+	private ArrayList<Carta> manoDelJugador;
+	
+	public Jugador(){
+		this.manoDelJugador = new ArrayList<Carta>();
 	}
 
+	public void nuevaCarta(Carta unaCarta) {
+		this.manoDelJugador.add(unaCarta);
+	}
+
+	
 	public int puntajeEnvido() {
-		// TODO Auto-generated method stub
-		return 0;
+		int puntajeARetornar = 0;
+		for (Carta unaCarta : this.manoDelJugador )
+			for (Carta otraCarta : this.manoDelJugador){
+				if (unaCarta != otraCarta){
+					int valorEnvido = this.calcularEnvido(unaCarta,otraCarta);	
+					if (valorEnvido > puntajeARetornar)
+						puntajeARetornar = valorEnvido;	
+				}
+			}
+		return puntajeARetornar;
 	}
+
+
+	private int calcularEnvido(Carta unaCarta, Carta otraCarta) {
+		if (! unaCarta.mismoPalo(otraCarta) )
+			return 0;
+		int valorRetorno = 20;
+		if ( unaCarta.getValor() < 8 )
+			valorRetorno = valorRetorno + unaCarta.getValor();
+		if ( otraCarta.getValor() < 8 )
+			valorRetorno = valorRetorno + otraCarta.getValor();
+		return valorRetorno;
+	}
+	
+	
 
 }
--- test/truco/modelo/JugadorTest.java
+++ test/truco/modelo/JugadorTest.java
@@ -15,7 +15,7 @@ public class JugadorTest {
 	}
 
 	@Test
-	public void testDevuelveCorretamenteElEnvidoTeniendoDosCartasDelMismoPalo() {
+	public void testPuntajeEnvidoDevuelveCorretamenteElEnvidoTeniendoDosCartasDelMismoPalo() {
 		// Se le pasa el 1 de Espada, el 7 de Esapda y el 3 de Oro. Envido = 28.
 		int envidoEsperado = 28;
 		this.jugador.nuevaCarta( new Carta(1,"espada") );
@@ -24,7 +24,60 @@ public class JugadorTest {
 		
 		int resultadoEnvido = this.jugador.puntajeEnvido();
 		
-		assertEquals(resultadoEnvido,envidoEsperado);
+		assertEquals(envidoEsperado,resultadoEnvido);
 	}
+	
+	@Test
+	public void testPuntajeEnvidoDevuelveCeroCuandoNoTieneCartasDelMismoPalo() {
+		// Se le pasa el 1 de Espada, el 2 de copa y el 3 de Oro. Envido = 0.
+		int envidoEsperado = 0;
+		this.jugador.nuevaCarta( new Carta(1,"espada") );
+		this.jugador.nuevaCarta( new Carta(2,"copa") );
+		this.jugador.nuevaCarta( new Carta(3,"oro") );
+		
+		int resultadoEnvido = this.jugador.puntajeEnvido();
+		
+		assertEquals(envidoEsperado,resultadoEnvido);
+	}
+	
+	@Test
+	public void testPuntajeEnvidoDevuelveElValorCorrectoCuandoTieneDosFiguras() {
+		// Se le pasa el 10 de Basto , el 12 de Basto y el 3 de Oro. Envido = 20.
+		int envidoEsperado = 20;
+		this.jugador.nuevaCarta( new Carta(10,"basto") );
+		this.jugador.nuevaCarta( new Carta(12,"basto") );
+		this.jugador.nuevaCarta( new Carta(3,"oro") );
+		
+		int resultadoEnvido = this.jugador.puntajeEnvido();
+		
+		assertEquals(envidoEsperado,resultadoEnvido);
+	}
+	
+	@Test
+	public void testPuntajeEnvidoDevuelveElValorCorrectoCuandoTieneSoloUnaFigura() {
+		// Se le pasa el 10 de Basto , el 4 de Basto y el 3 de Oro. Envido = 24.
+		int envidoEsperado = 24;
+		this.jugador.nuevaCarta( new Carta(10,"basto") );
+		this.jugador.nuevaCarta( new Carta(4,"basto") );
+		this.jugador.nuevaCarta( new Carta(3,"oro") );
+		
+		int resultadoEnvido = this.jugador.puntajeEnvido();
+		
+		assertEquals(envidoEsperado,resultadoEnvido);
+	}
+	
+	@Test
+	public void testPuntajeEnvidoDevuelveElMaximoEnvidoPosibleCuandoTieneTresCartasDelMismoPalo() {
+		// Se le pasa el 7 de Copa , el 12 de Copa y el 6 de Copa. Envido = 33.
+		int envidoEsperado = 33;
+		this.jugador.nuevaCarta( new Carta(7,"copa") );
+		this.jugador.nuevaCarta( new Carta(12,"copa") );
+		this.jugador.nuevaCarta( new Carta(6,"copa") );
+		
+		int resultadoEnvido = this.jugador.puntajeEnvido();
+		
+		assertEquals(envidoEsperado,resultadoEnvido);
+	}
+
 
 }
