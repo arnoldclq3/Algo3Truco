824b37aa624bec1f1935d2dfb840f86247601527
arnoldclq3 <arnoldclq3@gmail.com>
2015-12-10 00:37:49 -0300

    Se muestran los numeros de Ronda

--- src/truco/vista/VistaPuntaje.java
+++ src/truco/vista/VistaPuntaje.java
@@ -13,9 +13,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
-import truco.modelo.Equipo;
-import truco.modelo.Jugador;
-import truco.modelo.Mesa;
+import truco.modelo.*;
 
 public class VistaPuntaje implements Observer {
 
@@ -24,12 +22,16 @@ public class VistaPuntaje implements Observer {
 	private VBox ellos;
 	private Text puntajeNosotros;
 	private Text puntajeEllos;
+	private Text numeroRonda;
 	private Jugador miJugador;
+	private Ronda miRonda;
+	private int numero;
 	
 	public VistaPuntaje(Jugador jugador) {
 		
 		this.miJugador = jugador;
-		
+		this.numero = 1;
+		this.numeroRonda = this.nuevoTitulo("Ronda "+this.numero);
 		this.nosotros = this.nuevaCajaVertical();
 		this.ellos = this.nuevaCajaVertical();
 		this.puntajeNosotros = this.nuevoTitulo("0");
@@ -70,8 +72,9 @@ public class VistaPuntaje implements Observer {
 
 	private void agregarCajasVerticales() {
 		
-		this.vistaPuntaje.add(nosotros, 0, 0);
-		this.vistaPuntaje.add(ellos, 1, 0);
+		this.vistaPuntaje.add(this.numeroRonda, 0, 0);
+		this.vistaPuntaje.add(nosotros, 0, 1);
+		this.vistaPuntaje.add(ellos, 1, 1);
 	}
 	
 	public Node obtenerVista() {
@@ -96,6 +99,10 @@ public class VistaPuntaje implements Observer {
         Equipo equipo1 = laMesa.getEquipo1();
         Equipo equipo2 = laMesa.getEquipo2();
         
+        if ( this.miRonda == null ) {
+        	this.miRonda = laMesa.getRondaActual();
+        }
+        
         if ( equipo1.estaJugador(miJugador) ) {
         	this.puntajeNosotros.setText(equipo1.obtenerCantidadDePuntos()+"");
         	this.puntajeEllos.setText(equipo2.obtenerCantidadDePuntos()+"");
@@ -103,6 +110,12 @@ public class VistaPuntaje implements Observer {
         	this.puntajeNosotros.setText(equipo2.obtenerCantidadDePuntos()+"");
         	this.puntajeEllos.setText(equipo1.obtenerCantidadDePuntos()+"");
         }  
+        
+        if ( this.miRonda.estaTerminada() ) {
+        	this.miRonda = laMesa.getRondaActual();
+        	this.numero++;
+        	this.numeroRonda.setText("Ronda "+this.numero);
+        }
 	}
 	
 }
